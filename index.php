120a714c7e986447346be27a83589521345ac574
matvki <m.vecchi@it-students.fr>
2020-04-08 15:48:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="icon" type="image/png" href="./img/tree.png" />
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/header-footer.css">
+  <link rel="stylesheet" href="./css/index.css">
+  <title>Tree-Factorie</title>
+</head>
+
+<body>
+  <?php include_once './templates/header.php' ?>
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Tree-Facorie !</h1>
+        <h5>Tout autour de l'arbre</h5>
+      </div>
+      <img src="./img/feuilles-head.jpg" class="d-block w-100">
+      
+    </div>
+    <div class="carousel-item">
+      <img src="./img/Baobab.jpg" class="d-block w-100">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Tree-Facorie !</h1>
+        <h5>Tout autour de l'arbre</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/arbreCarousselle.jpg" class="d-block w-100">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Tree-Facorie !</h1>
+        <h5>Tout autour de l'arbre</h5>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Précédent</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Suivant</span>
+  </a>
+</div>
+
+
+  <div class="container">
+<p>Cette entreprise d'honnête travailleur vous propose le plus large choix en matière d'arbre en tout genre.</p>
+<p>Il vous faut un petit arbre d'intérieur ? <a href="#">nous avons ce qu'il vous faut !</a></p>
+<p>Il vous faut un arbre fruitier déjà adulte ? <a href="#">nous avons ce qu'il vous faut !</a></p>
+<p>Bref vous l'aurais compris nous pouvons vous avoir tout les abres existant.</p>
+</div>
+  
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ readMe.md
@@ -0,0 +1,2 @@
+Projet pour apprendre le Bootstrap
+Php 7.0 grace a une machine virtuel, ubuntu xenial 64
